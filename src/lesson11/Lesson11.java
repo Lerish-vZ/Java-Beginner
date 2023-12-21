629d4b0f320ffd2853db0f35905363202281f87c
Lerish-vZ <lerishavz@gmail.com>
2023-12-21 11:55:10 +0200

    Update

--- src/lesson11/Lesson11.java
+++ src/lesson11/Lesson11.java
@@ -44,6 +44,42 @@ public class Lesson11 {
 //        
 //        String fdate = today.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM));
 //        System.out.println("Formatted with MEDIUM FormatStyle: " + fdate);
-                
+
+        //TWO DIMENSIONAL ARRAYS
+        
+        /* 
+        Declaring a Two-Dimensional Array:
+        int [] [] yearlySale;
+        
+        SYNTAX: type [] [] array_identifier; 
+        
+        INSTANTIATING ARRAY 
+        instantiates a 2D array: 5 arrays og 4 elements each 
+        yearlySales = new int[5][4];
+        //5 years (Y axis) with 4 quaters (X axis)
+        
+        SYNTAX: array_identifier = new type [number_of_arrays] [length];
+        
+        When initializing [y axis index] [x axis index]
+        » yearlySales [0] [0] = 1000; sets it in the top left cell (Year 1, Quarter 1)
+        » yearlySales [0] [1] = 1500; sets it in the second cell from the top left (Year 1, Quarter 2)
+        */
+        
+        //WHILE LOOP 
+        
+        /*
+        SYNTAX: 
+          » while (boolean_expression) {
+                code_block
+            }
+        
+        */
+        System.out.println("/*");
+        int counter = 0;
+        while (counter < 3) {
+            System.out.println(" *");
+            counter++;
+        }
+        System.out.println("*/");
     }
 }
