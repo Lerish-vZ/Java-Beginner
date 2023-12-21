72e62089edfd12c1625e797f26e63df079acb864
Lerish-vZ <lerishavz@gmail.com>
2023-12-21 09:02:05 +0200

    Update

--- src/lesson11/Lesson11.java
+++ src/lesson11/Lesson11.java
@@ -4,15 +4,32 @@
  */
 package lesson11;
 
-import java.time.LocalDate;
+import java.time.LocalDate; //have to import but does it automatically
+import java.time.chrono.JapaneseDate;
+//above is called full qualfication
+//you qualify classes or packages (with their classes) in order to reference them directly in your code
 
+/* the import statement can be: 
+   for just the class in question 
+        java.time.LocalDate;
+   for all classes in the package 
+        java.time.*;
+*/
 /**
  *
  * @author lerishav
  */
 public class Lesson11 {
+    //if you need non-Gregorian type dates: 
+    //use the java.time.chrone classes
+    
     public static void main(String[] args) {
         LocalDate myDate = LocalDate.now();
         System.out.println("Today's date: " + myDate);
+
+        //CONVERTING A DATE 
+//        LocalDate myDate = LocalDate.now();
+        JapaneseDate jDate = JapaneseDate.from(myDate);
+        System.out.println("Japanese date: " + jDate);
     }
 }
