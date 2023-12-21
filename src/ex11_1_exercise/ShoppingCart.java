9891a9291a6d25fb7de5fcac95fc8514e5b0f155
Lerish-vZ <lerishavz@gmail.com>
2023-12-21 09:13:08 +0200

    Update

--- src/ex11_1_exercise/ShoppingCart.java
+++ src/ex11_1_exercise/ShoppingCart.java
@@ -2,17 +2,20 @@ package ex11_1_exercise;
 
 // import statements here:
 
+import java.time.LocalDateTime;
+
+
 
 public class ShoppingCart {
     public static void main(String[] args){
 	// Declare a LocalDateTime object, orderDate
-
+        LocalDateTime orderDate;
         
 	// Initialize the orderDate to the current date and time. Print it.
-
+         orderDate = LocalDateTime.now();
 
 	// Format orderDate using ISO_LOCAL_DATE; Print it.
-
+        
 
     }
 }
