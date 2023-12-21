aa30165f21b26563ed117c35295e7bfd6adc310b
Lerish-vZ <lerishavz@gmail.com>
2023-12-21 08:47:17 +0200

    Update

---
+++ src/lesson11/Lesson11.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lesson11;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author lerishav
+ */
+public class Lesson11 {
+    public static void main(String[] args) {
+        LocalDate myDate = LocalDate.now();
+        System.out.println("Today's date: " + myDate);
+    }
+}
