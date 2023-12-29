deeca3d601a61308ee46d2677dfa189f47d92182
Lerish-vZ <lerishavz@gmail.com>
2023-12-29 16:17:09 +0200

    Update

--- src/ex12_3_exercise/ShoppingCart.java
+++ src/ex12_3_exercise/ShoppingCart.java
@@ -3,7 +3,7 @@ package ex12_3_exercise;
 public class ShoppingCart {
     public static void main(String[] args) {
          // declare and instantiate a Shirt object using an Item reference type
-        Item item1 = new Item("Shirt", 2.99);
+        Item item1 = new Shirt(2.99, 'M', 'G');
         
         // call the display method on the object, then the getColor method
         item1.display();
