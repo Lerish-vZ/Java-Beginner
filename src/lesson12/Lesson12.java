c4e54d7157bb9e1c6280e9d83fb83dbc6786d985
Lerish-vZ <lerishavz@gmail.com>
2023-12-29 15:32:16 +0200

    Update

---
+++ src/lesson12/Lesson12.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lesson12;
+
+/**
+ *
+ * @author lerishav
+ */
+public class Lesson12 {
+    public static void main(String[] args) {
+        
+        //INHERITANCE
+        
+        /* 
+        Inheritance allows one class to be derived from another 
+          » a child inherits properties and behaviors of the parent
+          » a child class inherits the fields and method of a parent class. 
+        
+        The keyword *extends* creates the inheritance relationship
+        super refers to the supreclass
+        The subclass's own attributes gets set after invocation of the superclass constructor
+        
+        */
+
+    }
+}
