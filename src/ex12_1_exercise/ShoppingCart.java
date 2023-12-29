eb725c2fead41df41088b68842dd47fabff41abe
Lerish-vZ <lerishavz@gmail.com>
2023-12-29 15:43:38 +0200

    Update

--- src/ex12_1_exercise/ShoppingCart.java
+++ src/ex12_1_exercise/ShoppingCart.java
@@ -4,7 +4,7 @@ public class ShoppingCart {
     public static void main(String[] args){
 
 	// instantiate a Shirt object and call display() on the object reference
- 
-        
-    }
+      Shirt shirt = new Shirt(2.99, 'M', 'B');
+      shirt.display();
+   }
 }
