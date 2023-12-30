e3bd682ce1f926eaf964f0521f7712582ed2c034
Lerish-vZ <lerishavz@gmail.com>
2023-12-30 11:51:13 +0200

    Update

--- src/lesson13/Lesson13.java
+++ src/lesson13/Lesson13.java
@@ -14,6 +14,24 @@ public class Lesson13 {
         /* 
         All classes that do not explicitly extend another class automatically extend Object.
         
+        
+        
+        //Output an Object to the console
+        System.out.println(new Object());
+        //Output this StringBuilder object to the console 
+        System.out.println(new StringBuilder("Some text for the StringBuilder"));
+        //Output a class that does not override the toString() method
+        //System.out.println(new First());
+        //Output a class that *does* override the toString() method
+        //System.out.println(new Second());
+        
+        
+        INTERFACE 
+        An interface is similar to an abstract class except that:
+            » Methods are implicitly abstract (except default methods)
+            » A class does not extend it, but implemtnts it
+            » A class may implement more than one interface
         */
+                
     }     
 }
