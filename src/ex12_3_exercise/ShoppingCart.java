c4e5572e7ad6db28ac01f25dcac637530360048d
Lerish-vZ <lerishavz@gmail.com>
2024-01-03 13:41:45 +0200

    Update

--- src/ex12_3_exercise/ShoppingCart.java
+++ src/ex12_3_exercise/ShoppingCart.java
@@ -1,17 +1,18 @@
 package ex12_3_exercise;
 
 public class ShoppingCart {
+
     public static void main(String[] args) {
-         // declare and instantiate a Shirt object using an Item reference type
-        Item item1 = new Shirt(2.99, 'M', 'G');
-        
+        // declare and instantiate a Shirt object using an Item reference type
+        Item item1 = new Shirt(2.99, 'M', 'b');
+        //Item item1 = new Item();
+
         // call the display method on the object, then the getColor method
         item1.display();
-        if (item1 instanceof Shirt){
-            String color = ((Shirt)item1).getColor();
+        if (item1 instanceof Shirt) {
+            String color = ((Shirt) item1).getColor();
             System.out.println("Color of " + item1.getDesc() + ": " + color);
-        } 
-        else {
+        } else {
             System.out.println("Item is not a shirt.");
         }
     }
